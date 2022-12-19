309e6989d8f22302ab00361444a652e1771b8bd9
kimjuii <cathy033077@gmail.com>
2022-12-20 07:21:26 +0900

    feat: korean cleaner

---
+++ korean/ko_cleaner.py
@@ -0,0 +1,107 @@
+#https://github.com/coqui-ai/TTS/tree/dev/TTS/tts/utils/text/korean
+# https://github.com/Deepest-Project/MelNet/blob/master/text/korean.py
+
+import re
+from ko_normalize import normalize
+from jamo import hangul_to_jamo, h2j, j2h, hcj_to_jamo, is_hcj
+from jamo.jamo import _jamo_char_to_hcj
+
+PAD = '_'
+EOS = '~'
+PUNC = '!\'(),-.:;?'
+SPACE = ' '
+
+'''
+한국어 초성, 중성, 종성 구분해 유니코드로 변경
+제주 방언에는 아래아, 쌍아래아 사용됨
+ᆞ (아래아) 0x119
+ᆢ (쌍아래아) 0x11A2
+'''
+
+LEADS = [chr(_) for _ in range(0x1100, 0x1113)]
+JAMO_LEADS = "".join(LEADS)
+
+VOWELS = [chr(_) for _ in range(0x1161, 0x1176)]
+VOWELS.extend([chr(_) for _ in [0x119, 0x11A2]])
+JAMO_VOWELS = "".join(VOWELS)
+
+TAILS = [chr(_) for _ in range(0x11A8, 0x11C3)]
+JAMO_TAILS = "".join(TAILS)
+
+VALID_CHARS = JAMO_LEADS + JAMO_VOWELS + JAMO_TAILS + PUNC + SPACE
+ALL_SYMBOLS = PAD + EOS + VALID_CHARS
+
+char_to_id = {c: i for i, c in enumerate(ALL_SYMBOLS)}
+id_to_char = {i: c for i, c in enumerate(ALL_SYMBOLS)}
+
+
+def is_lead(char):
+    return char in JAMO_LEADS
+
+def is_vowel(char):
+    return char in JAMO_VOWELS
+
+def is_tail(char):
+    return char in JAMO_TAILS
+
+def get_mode(char):
+    if is_lead(char):
+        return 0
+    elif is_vowel(char):
+        return 1
+    elif is_tail(char):
+        return 2
+    else:
+        return -1
+
+def _get_text_from_candidates(candidates):
+    if len(candidates) == 0:
+        return ""
+    elif len(candidates) == 1:
+        return _jamo_char_to_hcj(candidates[0])
+    else:
+        return j2h(**dict(zip(["lead", "vowel", "tail"], candidates)))
+
+def jamo_to_korean(text):
+    text = h2j(text)
+
+    idx = 0
+    new_text = ""
+    candidates = []
+
+    while True:
+        if idx >= len(text):
+            new_text += _get_text_from_candidates(candidates)
+            break
+
+        char = text[idx]
+        mode = get_mode(char)
+
+        if mode == 0:
+            new_text += _get_text_from_candidates(candidates)
+            candidates = [char]
+        elif mode == -1:
+            new_text += _get_text_from_candidates(candidates)
+            new_text += char
+            candidates = []
+        else:
+            candidates.append(char)
+
+        idx += 1
+    return new_text
+  
+def compare_sentence_with_jamo(text1, text2):
+    return h2j(text1) != h2j(text2)
+
+def tokenize(text, as_id=False):
+    # "jamo" package의 hangul_to_jamo를 이용하여 한글 string을 초성/중성/종성으로 분리
+    text = normalize(text)
+    tokens = list(hangul_to_jamo(text)) # '존경하는'  --> ['ᄌ', 'ᅩ', 'ᆫ', 'ᄀ', 'ᅧ', 'ᆼ', 'ᄒ', 'ᅡ', 'ᄂ', 'ᅳ', 'ᆫ', '~']
+
+    if as_id:
+        return [char_to_id[token] for token in tokens] + [char_to_id[EOS]]
+    else:
+        return [token for token in tokens] + [EOS]
+
+def tokenizer_fn(iterator):
+    return (token for x in iterator for token in tokenize(x, as_id=False))
---
+++ korean/ko_normalize.py
@@ -0,0 +1,146 @@
+﻿# coding: utf-8
+# Code based on https://github.com/carpedm20/multi-speaker-tacotron-tensorflow/blob/master/text/korean.py
+import re
+import ast
+from ko_dictionary import english_dictionary, etc_dictionary, num_to_kor, unit_to_kor
+
+
+def normalize(text):
+    text = text.strip()
+    text = re.sub("[⺀-⺙⺛-⻳⼀-⿕々〇〡-〩〸-〺〻㐀-䶵一-鿃豈-鶴侮-頻並-龎]", "", text)
+    text = normalize_with_dictionary(text, etc_dictionary)
+    text = normalize_english(text)
+    text = text.lower()
+    return text
+
+def normalize_with_dictionary(text, dic):
+    if any(key in text for key in dic.keys()):
+        pattern = re.compile("|".join(re.escape(key) for key in dic.keys()))
+        return pattern.sub(lambda x: dic[x.group()], text)
+    return text
+
+def normalize_english(text):
+    def fn(m):
+        word = m.group()
+        if word in english_dictionary:
+            return english_dictionary.get(word)
+        return word
+
+    text = re.sub("([A-Za-z]+)", fn, text)
+    return text
+
+quote_checker = """([`"'＂“‘])(.+?)([`"'＂”’])"""
+
+def normalize_quote(text):
+    def fn(found_text):
+        from nltk import sent_tokenize # NLTK doesn't along with multiprocessing
+
+        found_text = found_text.group()
+        unquoted_text = found_text[1:-1]
+
+        sentences = sent_tokenize(unquoted_text)
+        return " ".join(["'{}'".format(sent) for sent in sentences])
+
+    return re.sub(quote_checker, fn, text)
+
+number_checker = "([+-]?\d[\d,]*)[\.]?\d*"
+count_checker = "(시|명|가지|살|마리|포기|송이|수|톨|통|점|개|벌|척|채|다발|그루|자루|줄|켤레|그릇|잔|마디|상자|사람|곡|병|판)"
+
+def normalize_number(text):
+    text = normalize_with_dictionary(text, unit_to_kor)
+    text = re.sub(number_checker + count_checker,
+            lambda x: number_to_korean(x, True), text)
+    text = re.sub(number_checker,
+            lambda x: number_to_korean(x, False), text)
+    return text
+
+
+num_to_kor1 = [""] + list("일이삼사오육칠팔구")
+num_to_kor2 = [""] + list("만억조경해")
+num_to_kor3 = [""] + list("십백천")
+
+#count_to_kor1 = [""] + ["하나","둘","셋","넷","다섯","여섯","일곱","여덟","아홉"]
+count_to_kor1 = [""] + ["한","두","세","네","다섯","여섯","일곱","여덟","아홉"]
+
+count_tenth_dict = {
+        "십": "열",
+        "두십": "스물",
+        "세십": "서른",
+        "네십": "마흔",
+        "다섯십": "쉰",
+        "여섯십": "예순",
+        "일곱십": "일흔",
+        "여덟십": "여든",
+        "아홉십": "아흔",
+}
+
+def number_to_korean(num_str, is_count=False):
+    if is_count:
+        num_str, unit_str = num_str.group(1), num_str.group(2)
+    else:
+        num_str, unit_str = num_str.group(), ""
+    
+    num_str = num_str.replace(',', '')
+    num = ast.literal_eval(num_str)
+
+    if num == 0:
+        return "영"
+
+    check_float = num_str.split('.')
+    if len(check_float) == 2:
+        digit_str, float_str = check_float
+    elif len(check_float) >= 3:
+        raise Exception(" [!] Wrong number format")
+    else:
+        digit_str, float_str = check_float[0], None
+
+    if is_count and float_str is not None:
+        raise Exception(" [!] `is_count` and float number does not fit each other")
+
+    digit = int(digit_str)
+
+    if digit_str.startswith("-"):
+        digit, digit_str = abs(digit), str(abs(digit))
+
+    kor = ""
+    size = len(str(digit))
+    tmp = []
+
+    for i, v in enumerate(digit_str, start=1):
+        v = int(v)
+
+        if v != 0:
+            if is_count:
+                tmp += count_to_kor1[v]
+            else:
+                tmp += num_to_kor1[v]
+
+            tmp += num_to_kor3[(size - i) % 4]
+
+        if (size - i) % 4 == 0 and len(tmp) != 0:
+            kor += "".join(tmp)
+            tmp = []
+            kor += num_to_kor2[int((size - i) / 4)]
+
+    if is_count:
+        if kor.startswith("한") and len(kor) > 1:
+            kor = kor[1:]
+
+        if any(word in kor for word in count_tenth_dict):
+            kor = re.sub(
+                    '|'.join(count_tenth_dict.keys()),
+                    lambda x: count_tenth_dict[x.group()], kor)
+
+    if not is_count and kor.startswith("일") and len(kor) > 1:
+        kor = kor[1:]
+
+    if float_str is not None:
+        kor += "쩜 "
+        kor += re.sub('\d', lambda x: num_to_kor[x.group()], float_str)
+
+    if num_str.startswith("+"):
+        kor = "플러스 " + kor
+    elif num_str.startswith("-"):
+        kor = "마이너스 " + kor
+
+    return kor + unit_str
